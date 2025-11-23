184727a1bb43317f710b1d35cdd3f9f0be05a00b
Chaithali R <chaii@Chaithalis-MA3.local>
2025-11-23 21:38:41 +0530

    Express server running with /ping route

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const cors=require("cors");
+const dotenv=require("dotenv");
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.get("/ping",(req,res)=> {
+    console.log("Ping route hit");
+    res.json({message:"pong"});
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is ruuning on port ${PORT}`);
+});
