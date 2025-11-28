cabd43bebafe396acb05e5904d4af60d7c31e879
rchaithali <chaithalir523@gmail.com>
2025-11-28 20:05:59 +0530

    bcrypt and login added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const bcrypt=require("bcrypt");
 const User= require("./models/User");
 const connectDB = require("./db");
 const express=require("express");
@@ -11,22 +12,47 @@ app.get("/ping",(req,res)=> {
     console.log("Ping route hit");
     res.json({message:"pong"});
 });
-app.post("/register", async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+app.post("/register", async(req,res) => {
+    try{const {name,email,password}=req.body;
+    const hashedPassword=await bcrypt.hash(password,10);
+    const newUser=new User({
+        name,email,password:hashedPassword
 
-        const newUser = await User.create({
-            name,
-            email,
-            password
+    });
+    await newUser.save();
+    res.json({
+        message:"User registered successfully(password protected",
+        user:{
+            name:newUser.name,
+            email:newUser.email,
+            id:newUser._id
+        }
+    });
+} catch(err){
+    res.status(500).json({error:"Error registering user",details:err});
+}
+});
+app.post("/login", async(req,res) => {
+    try{
+        const{email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(404).json({erroe:"User not found"});
+        }
+        const isMatch= await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(404).json({error:"Invalid Password"});
+        }
+        res.json({
+            message:"Login succesful",
+            user:{
+                name:user.name,
+                email:user.email,
+                id:user._id
+            }
         });
-
-        res.status(201).json({
-            message: "User registered successfully",
-            user: newUser
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    } catch(err){
+        res.status(500).json({error:"Login failed",details:err});
     }
 });
 
